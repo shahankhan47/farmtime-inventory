77b73ffc38a52e7a461e731a868ff1f4e29c0c1f
Shahan Khan <prince47khan@gmail.com>
2024-05-21 02:21:35 +0530

    correct backend url

--- frontend/src/data/api.js
+++ frontend/src/data/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = "https://farmtime-backend.vercel.app"
+const backendUrl = "https://farmtime-backend.vercel.app/api"
 
 const createService = async (formData) => {
     try {
